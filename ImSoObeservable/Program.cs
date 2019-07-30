32406ede86bd0fa3b5e6accd9499b67eb6fd8ee1
asandriss <fedorhajdu@gmail.com>
2019-07-31 00:15:17 +0400

    Few options when generating observables

--- ImSoObeservable/Program.cs
+++ ImSoObeservable/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
 using System.Threading;
+using System.Threading.Tasks;
 
 
 namespace ImSoObeservable
@@ -12,7 +14,7 @@ namespace ImSoObeservable
         static void Main(string[] args)
         {
             var random = new Random((int)DateTime.Now.Ticks);
-            IObservable<long> sequence = Observable.Interval(TimeSpan.FromMilliseconds(500));
+            IObservable<int> sequence = Generate();     //Interval();          // Create();    
 
             sequence.Subscribe
             (
@@ -22,17 +24,48 @@ namespace ImSoObeservable
             );
 
             Console.WriteLine($"Hello World! - ThreadID = {Thread.CurrentThread.ManagedThreadId}");
+            Console.ReadLine();
+            //while (true)
+            //{
+            //    Console.WriteLine($"Going to sleep now - ThreadID = {Thread.CurrentThread.ManagedThreadId}");
+            //    Thread.Sleep(1250);
+            //    Console.WriteLine($"Just woke up! doing heavy maths - ThreadID = {Thread.CurrentThread.ManagedThreadId}");
+            //    for (int i = 0; i < 10000000; i++)
+            //    {
+            //        var result = (i * random.Next(10000)) / ((random.Next(99)+1)/100m);
+            //    }
+            //}
+        }
 
-            while (true)
+        private static IObservable<long> Interval()
+        {
+            return Observable.Interval(TimeSpan.FromMilliseconds(500));
+        }
+
+        private static IObservable<int> Create()
+        {
+            // complete control over creation
+            return Observable.Create<int>(obs =>
             {
-                Console.WriteLine($"Going to sleep now - ThreadID = {Thread.CurrentThread.ManagedThreadId}");
-                Thread.Sleep(1250);
-                Console.WriteLine($"Just woke up! doing heavy maths - ThreadID = {Thread.CurrentThread.ManagedThreadId}");
-                for (int i = 0; i < 10000000; i++)
-                {
-                    var result = (i * random.Next(10000)) / ((random.Next(99)+1)/100m);
-                }
-            }
+                obs.OnNext(1);
+                obs.OnNext(22);
+                obs.OnNext(42);
+                obs.OnCompleted();
+                return Disposable.Empty;
+            });
+        }
+
+        private static IObservable<int> Generate()
+        {
+            // generate observable - set seed, break condition (false for infinite), increment, selector
+            return Observable.Generate
+            (
+                1,
+                o => o <= 5,
+                o => o + 1,
+                o => o
+            ).Delay(TimeSpan.FromMilliseconds(500));
+            // interesting that this sequence executes on main thread, while Interval executes in background.
         }
     }
 }
